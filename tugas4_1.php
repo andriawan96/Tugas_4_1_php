c1a6ed613e59ec5e860c904c42bb5f1f4c94f8a8
andriawan <alhaqandriawan@gmail.com>
2020-02-26 10:04:39 +0700

    Tugas 4 php
    Tugas ini di kumpulkan untuk niomic

---
+++ tugas4_1.php
@@ -0,0 +1,12 @@
+<?php
+$beli = 8000;
+$jual = 7500;
+$untung = 300;
+
+$perhitungan = ($beli - $jual) / $untung;
+
+echo "Harga beli = Rp 8000<br>
+      Harga jual = Rp 7500<br>
+      Keuntungan = Rp 300<br>
+      Berapa jumlah kambing yang di beli ? Jawaban : $perhitungan ekor";
+ ?>
---
+++ tugas4_2.php
@@ -0,0 +1,11 @@
+<?php
+
+$saluran = 523;
+$terpakai = 8891;
+$rata2 = $terpakai / $saluran;
+    echo "Jumlah Saluran = 523<br>
+          Jumlah Terpakai = 8891<br>
+          Rata-rata Pemakaian = ...?<br>
+          Jawaban : $rata2 liter/keluarga
+          ";
+?>
